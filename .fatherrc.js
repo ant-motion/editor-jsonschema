48cefcfe423558577cfd2afa891431c8d624162d
jljsj <liangjun.jiang@alipay.com>
2019-06-11 17:39:59 +0800

    update

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,4 +1,10 @@
 export default {
+  cjs: 'babel',
+  esm: { type: 'babel', importLibToEs: true },
+  preCommit: {
+    eslint: true,
+    prettier: true,
+  },
   extraBabelPlugins: [
     ['babel-plugin-import', {
       libraryName: 'antd',
